5297b56bcbdf1ebf587060638a4a028cceb0d62f
chukwueku <83511919+chukwueku@users.noreply.github.com>
2026-08-29 18:12:56 +0100

    chore: update EcoMastery-app-debug.apk binary with latest Google AI Studio changes

--- fix_studyguide3.cjs
+++ fix_studyguide3.cjs
@@ -3,6 +3,6 @@ const fs = require('fs');
 let content = fs.readFileSync('src/components/StudyGuide.tsx', 'utf8');
 
 // Replace the mangled part
-const mangledRegex = /const preprocessLaTeX = \([\s\S]*?\.replace\(\/\\\\\\(\/g, '/;
+const mangledRegex = /const preprocessLaTeX = \([\s\S]*?\.replace\(\/\\\\\\\( \/g, '/;
 content = content.replace(mangledRegex, "");
 // Wait, I need to be precise.
